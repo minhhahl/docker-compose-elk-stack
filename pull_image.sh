f8a6a1281bc50f74d4c951f93d15c012a012d2e4
Pham Minh Ha <minhhahl@gmail.com>
2021-09-25 23:04:04 +0700

    add script to pull all images

---
+++ pull_image.sh
@@ -0,0 +1,14 @@
+source .env
+
+docker pull docker.elastic.co/elasticsearch/elasticsearch:${ELK_VERSION}
+docker pull docker.elastic.co/logstash/logstash:${ELK_VERSION}
+docker pull docker.elastic.co/kibana/kibana:${ELK_VERSION}
+docker pull docker.elastic.co/beats/filebeat:${ELK_VERSION}
+docker pull docker.elastic.co/beats/metricbeat:${ELK_VERSION}
+docker pull docker.elastic.co/beats/auditbeat:${ELK_VERSION}
+docker pull docker.elastic.co/beats/heartbeat:${ELK_VERSION}
+docker pull docker.elastic.co/beats/journalbeat:${ELK_VERSION}
+docker pull docker.elastic.co/beats/elastic-agent:${ELK_VERSION}
+docker pull docker.elastic.co/beats/packetbeat:${ELK_VERSION}
+docker pull docker.elastic.co/enterprise-search/enterprise-search:${ELK_VERSION}
+docker pull docker.elastic.co/apm/apm-server:${ELK_VERSION}
