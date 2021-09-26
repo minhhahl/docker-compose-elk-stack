c9d6ac0a85bd8985bf9673f6eeb4377d938c5ed2
Pham Minh Ha <minhhahl@gmail.com>
2021-09-26 10:50:02 +0700

    test elastic-agent and Feet

--- pull_image.sh
+++ pull_image.sh
@@ -9,6 +9,7 @@ docker pull docker.elastic.co/beats/auditbeat:${ELK_VERSION}
 docker pull docker.elastic.co/beats/heartbeat:${ELK_VERSION}
 docker pull docker.elastic.co/beats/journalbeat:${ELK_VERSION}
 docker pull docker.elastic.co/beats/elastic-agent:${ELK_VERSION}
+# docker pull docker.elastic.co/beats/elastic-agent-complete:${ELK_VERSION}
 docker pull docker.elastic.co/beats/packetbeat:${ELK_VERSION}
 docker pull docker.elastic.co/enterprise-search/enterprise-search:${ELK_VERSION}
 docker pull docker.elastic.co/apm/apm-server:${ELK_VERSION}
